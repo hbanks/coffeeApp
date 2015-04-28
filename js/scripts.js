d9e1af9928e9918987f634be787fc4ca4c7ea83b
Heather Banks <heather.banks03@gmail.com>
2015-04-28 16:52:50 -0400

    finished the styling; added a scrollTop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,17 @@
 
-var app = angular.module("myApp", ['ui.router']);
-
-app.config(function($stateProvider){
-	$stateProvider.state('index', {
-		url: '',
-		controller: 'mainController'
-		// templateUrl: 'js/templates/testtemp.html'
-	});
-});
+var app = angular.module("myApp", []);
 
 app.factory('myData', function($http, $q){
 	var clientID = 'WNANNEHBRREEDHVD4IVAOCHGT4SHT1Q4ZM0FWM3IQPEA2NNB';
 	var clientSecret = '0QV0SFDCT3VULM5RBGCLQYRTLNC2PWG4TPUOR0WXQX3R5SPF';
   var apiUrl = 'https://api.foursquare.com/v2/venues/explore?';
   var key = 'client_id=' + clientID + '&client_secret=' + clientSecret;
-  // var near = 'Toronto';
-  // var search = '&v=20130815&near=' + near + '&query=coffee';
 
 	return{
-		// getCoffee: function(){
-		// 	//creating a function that will be deferred until the success of my ajax call
-		// 	var def = $q.defer();
 
-		// 	//ajax call
-		// 	$http.get(apiUrl + key + search)
-		// 		.success(def.resolve)
-		// 		.error(def.reject);
-
-		// 		// returning whether the promise was resolved or rejected
-		// 		return def.promise;
-		// },
 		search: function(location){
 			//creating a function that will be deferred until the success of my ajax call
 			var def = $q.defer();
-				console.log(location);
 
 			//ajax call
 			$http.get(apiUrl + key + '&v=20130815&near=' + location + '&query=coffee' + '&venuePhotos=1')
@@ -51,21 +29,9 @@ app.factory('myData', function($http, $q){
 
 app.controller('mainController', function($scope, myData, $q){
 
-	// myData.search($scope.searchLocation).then(function(data){
-	// 	$scope.data = data.response.groups[0].items
-	// 	console.log($scope.data);
-	// 	$scope.movies = data
-
-
-	// });
 		$scope.search = function(){
 			myData.search($scope.searchLocation).then(function(data){
-				$scope.data = data.response.groups[0].items;
-				// $scope.dataPhoto = data.response.groups[0].items;
-				// $scope.prefix = $scope.dataPhoto.venue.featuredPhotos.items
-				// $scope.photo = $scope.dataPhoto.prefix + $scope.dataPhoto.height + $scope.dataPhoto.suffix;
-				console.log($scope.data)
-
+				$scope.data = data.response.groups[0].items;		
 			});
 		};
 
@@ -81,4 +47,13 @@ app.filter('httpReplace', function() {
 			return text.replace(/https?:\/\//, '');
 		}
 	}
+});
+
+// when search button is clicked, scroll to the main section
+$('.searchButton').on('click', function(event) {
+
+    $('html, body').animate({
+    scrollTop: $("#main").offset().top
+}, 1250);
+
 });
